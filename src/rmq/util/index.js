6368cd915443cef86ea10e87b707f63fa541d05b
jowlee <joeylee0925@gmail.com>
2016-12-17 16:22:51 -0500

    minor fixes: const and docs

--- src/rmq/util/index.js
+++ src/rmq/util/index.js
@@ -13,14 +13,19 @@ export async function rpc(queue: string, message: string): Promise<string> {
   return reply;
 }
 
-export async function rpcReply(queue: string, cb: any) {
+export async function rpcReply(queue: string, cb: function) {
+  /**
+   * Reponds to a remote procedure call (rpc)
+   * queue: String name of the queue
+   * cb: Callback funtion takes in the message and returns a promise
+   */
   amqplib.connect(config.rabbitmqAddress, function (err, connection) {
     connection.createChannel(function(err, ch) {
           ch.assertQueue(queue, {durable: false});
           ch.prefetch(1);
           ch.consume(q, async (msg) => {
-            let content = msg.content.toString();
-            let response = await cb(content);
+            const content = msg.content.toString();
+            const response = await cb(content);
             ch.sendToQueue(msg.properties.replyTo,
               new Buffer(response));
             ch.ack(msg);
