4536d0a9a3c0fd953cc587ea4f755827c45862d5
seveibar <seveibar@gmail.com>
2016-12-19 12:10:11 -0500

    fix to type hint and bad address variable

--- src/rmq/util/rabbitmq.js
+++ src/rmq/util/rabbitmq.js
@@ -2,7 +2,7 @@ import { connect as rabbitmqConnect } from 'amqplib';
 import config from '../../config';
 
 async function createRabbitMQConnection() {
-  const conn = await rabbitmqConnect(config.rabbitmqAddress);
+  const conn = await rabbitmqConnect(config.rabbitmq.address);
   return conn;
 }
 
